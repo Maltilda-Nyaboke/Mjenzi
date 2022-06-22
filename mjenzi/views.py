457d661da5ad93d5398223703908058fd672959d
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-23 02:49:58 +0300

    style:add register function

--- mjenzi/views.py
+++ mjenzi/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index.html')
+    else:        
+        form = RegisterForm()
+    return render(request,'register.html',{'form':form})      
