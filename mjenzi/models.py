fe18bf120773260849b264f9dae89b280d7bf021
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-23 02:48:15 +0300

    style:add register form

--- mjenzi/models.py
+++ mjenzi/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
+    name = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    about = models.TextField(blank=True)
+    role = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Rating(models.Model):    
+
+
